3339a9e2fa62b8fdcf160b0eda51e19806d21f93
chris <teckchris@gmail.com>
2013-11-23 23:28:03 -0500

    Changing lines to under 80 lines for PEP8.

--- hint_game.py
+++ hint_game.py
@@ -2,51 +2,60 @@ import random
 
 #This is HINT. Like "CLUE", but not copyright infringing.
 #Here we have a list of options for a murder.
-Char = ["Mrs. Scarlet", "Col. Mustard", "Ms. White", "Mr. Green", "Rebecca Black", "Blue Man Group"]
-Weapon = ["Gun", "Candle Stick", "Lead Pipe", "Antifreeze", "Wrench", "Rope", "Arson"]
-Rooms = ["Library", "Kitchen", "Dining Room", "Garage", "Office", "Make-out Shed", "Auxiliary Basement"]
+Char = ["Mrs. Scarlet", "Col. Mustard", "Ms. White",
+        "Mr. Green", "Rebecca Black", "Blue Man Group"]
+
+Weapon = ["Gun", "Candle Stick", "Lead Pipe",
+          "Antifreeze", "Wrench", "Rope", "Arson"]
+
+Rooms = ["Library", "Kitchen", "Dining Room", "Garage",
+         "Office", "Make-out Shed", "Auxiliary Basement"]
 
 #This Dictionary is going randomly select from the options to guess at.
-Murder = {"person" : '', "killingthingy" : '', "place": ''}
+Murder = {"person": random.choice(Char),
+          "killingthingy": random.choice(Weapon),
+          "place": random.choice(Rooms)}
 
 #Here we populate the dict with the murder facts.
-Murder["person"] = random.choice(Char)
-Murder["killingthingy"] = random.choice(Weapon)
-Murder["place"] = random.choice(Rooms)
 
+print("""
+ Isn't this a lovely party? Well, somebody is dead now, let's figure out who.
 
-print "Isn't this a lovely party? Well, somebody is dead now, let's figure out who."
-print
-print "Your guests are: " + ", ".join(Char)
-print
-print "The weapons on hand are: " + ", ".join(Weapon)
-print
-print "Your weird house has: " + ", ".join(Rooms)
-print
-print "So, there you have it. Start guessing."
-print "Be careful! If you take too long to figure it out, the murderer will get nervous and kill you too!"
-print
-print
+ Your guests are: {0}
+
+ The weapons on hand are: {1}
+
+ Your weird house has: {2}
+
+ So, there you have it. Start guessing.
+ Be careful! If you take too long to figure it out,\
+the murderer will get nervous and kill you too!
+""".format(", ".join(Char), ", ".join(Weapon), ", ".join(Rooms)))
+
 
 #random number of chances you get to guess:
 hints_left = random.randint(25, 36)
 
-#The idea is that after a set of guessing, the number of guesses left counts down.
+#The idea is that after a set of guessing the number of guesses left decreases.
 #The guesses get checked against the murder dict.
 #If any guess is wrong, give a hint for a wrong guess.
 #i.e. who = Mrs. Scarlet "but Mrs. Scarlet was with me the whole time!"
 #When all guesses are right, game won.
-#When no guesses are left, game over. Kinda like hangman?? but a little more sinister?
+#When no guesses are left, game over.
+#Kinda like hangman?? but a little more sinister?
 
 
-def ask_me_anything(lives):
-    correctness = False
+def ask_me_anything():
     who = raw_input("Who did it? ")
     what = raw_input("With what? ")
     where = raw_input("Where? ")
+    return who, what, where
 
+
+def run_game(lives):
     while lives > 0:
         wrongers = []
+        who, what, where = ask_me_anything()
         if who != Murder['person']:
             wrongers.append(who)
         if what != Murder['killingthingy']:
@@ -54,15 +63,14 @@ def ask_me_anything(lives):
         if where != Murder['place']:
             wrongers.append(where)
         if len(wrongers) == 0:
-            print "YOU WIN!"
-            correctness = True
-            lives = 0
+            print("YOU WIN!")
+            return
         else:
-            print str(random.choice(wrongers)) + " isn't right!"
-            print
+            print (str(random.choice(wrongers)) + " isn't right!\n")
             lives -= 1
-            ask_me_anything(hints_left)
-    if correctness is False:
-        print "You died. " + Murder["person"] + " killed you with " + Murder["killingthingy"] + " in the " + Murder["place"] + "."
+    "You died. \
+    {0} killed you with {1} in the {2}.".format(Murder["person"],
+                                                Murder["killingthingy"],
+                                                Murder["place"])
 
-ask_me_anything(hints_left)
+run_game(hints_left)
